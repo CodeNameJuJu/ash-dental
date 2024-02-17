20a2d10fac05df2f73241ea843adae1e46b8f2ac
NathanKru <nathankruger21@gmail.com>
2024-02-17 17:54:00 +0200

    Sub Services pages layout

---
+++ src/components/subServices/generalDentistry.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+import "../../Main.css";
+import { Link, useLocation } from 'react-router-dom';
+
+
+function GeneralDentistry() {
+    function Navigate({ name, path }) {
+        return (
+            <Link className="nav-item nav-link" to={path} title={name}>
+                {name}
+            </Link>
+        );
+    }
+    return (
+        <div>
+            <hr ></hr>
+            <nav class="navbar nav-bg navbar-expand-sm "> <button class="navbar-toggler" type="button" data-target="#navigation"> <span class="navbar-toggler-icon"></span> </button>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="sub-nav-item active">
+                            <Navigate name={'Cosmetic Dentistry'} path={'/cosmeticDentistry'} /> </li>
+                        <li class="sub-nav-item">
+                            <Navigate name={'Teeth Whitening'} path={'/teethWhitening'} /> </li>
+                        <li class="sub-nav-item"><Navigate name={'Crowns and Veneers'} path={'/crowns'} />  </li>
+                        <li class="sub-nav-item"> <Navigate name={'Dental Implants'} path={'/dental'} />  </li>
+                        <li class="sub-nav-item"><Navigate name={'Orthodontics'} path={'/orthodontics'} /> </li>
+                        <li class="sub-nav-item"> <Navigate name={'General Dentistry'} path={'/generalDentistry'} />  </li>
+                        <li class="sub-nav-item"> <Navigate name={'24 Hour Emergencies'} path={'/emergencies'} /> </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="row serv-top">
+                <div class=" col-7 text-center">
+                    <h1 class="gold text-start">Comprehensive General Dentistry</h1>
+                    <h3 class="brown text-start ">Personalized Care for Optimal Oral Health</h3>
+                    <br/>
+                    <p class="lead text-start">Experience personalised care and optimal oral health with our comprehensive range of general
+                            dental services for the whole family that go beyond routine check-ups and cleanings. We believe in
+                            crafting customized treatment plans tailored to individual needs, reflecting our commitment to
+                            excellence in every service we offer.</p>
+                        <br/>
+                        <p class="lead text-start"> From dental restorations to general maintenance, we ensure
+                            that every aspect of your oral health is meticulously addressed.</p>
+                        <br/>
+                </div>
+
+                <div class="col-5">
+                    <div class="serv2-image-block">
+                        <img class="serv2-image" src="/Images/ServicesLandscape/TeethWhitening.png" alt="2 description"></img>
+                        <div class="serv2-overlay-block"></div>
+                    </div>
+                </div>
+            </div>
+
+            <br></br>
+
+            <div class="row serv-top">
+                <br/>
+                <br/>
+                    <h3>Routine Check-ups and Cleanings:</h3>
+                    <p class="lead text-start"> Routine Check-ups and Cleanings are more than just
+                        appointments – they are essential steps towards ensuring your long-term oral health. Our dedicated
+                        team of professionals takes a personalized approach to every check-up, prioritizing preventive care
+                        to detect and address any concerns at an early stage. From thorough examinations to gentle
+                        cleanings, scaling and polish - our routine appointments are designed to keep your smile vibrant and
+                        healthy. Embrace the reassurance that comes with regular dental check-ups and cleanings, where
+                        your oral well-being is our top priority.
+                    </p>
+                    <br/>
+                    <h3>Root Canal Therapy:</h3>
+                    <p class="lead text-start"> Experience advanced Endodontic treatments at ASH Dental, where we redefine
+root canal therapy with precision and innovation to alleviate the throbbing pain. Endodontic
+procedures, commonly known as root canal treatments, involve the meticulous removal of infected
+or damaged pulp from within the tooth (dental abscess), followed by thorough cleaning,
+disinfection, and the sealing of the root canal.
+                    </p>
+                    <br></br>
+                    <p class="lead text-start">
+                        At ASH Dental, we employ cutting-edge 3D imaging technology to enhance the diagnostic and
+                        treatment process, providing a comprehensive view of the tooth&#39;s internal structure. This ensures
+                        unparalleled accuracy and precision in addressing complex root canal issues. Our dedicated team of
+                        professionals combines expertise with advanced techniques to make Endodontic treatments a
+                        seamless and effective experience, promoting long-term oral health. Trust ASH Dental for state-of-
+                        the-art Endodontic care that goes beyond conventional root canal therapy, ensuring optimal results
+                        for your dental well-being.
+                    </p>
+                    <br/>
+                    <h3>Tooth Extractions:</h3>
+                    <p class="lead text-start"> Our tooth extraction procedures ensure a seamless and comfortable process.
+                        Whether it&#39;s the removal of a damaged, decayed or painful tooth the extraction of wisdom teeth,
+                        our skilled professionals prioritize precision and patient well-being. At ASH Dental, we leverage
+                        cutting-edge techniques to enhance the accuracy and efficiency of tooth extraction. Advanced
+                        imaging technology, including 3D imaging, allows us to thoroughly assess the tooth and surrounding
+                        structures, ensuring a comprehensive approach to each extraction. Trust our dedicated team to
+                        make tooth extraction a smooth and precise experience.
+                    </p>
+                    <br/>
+                    <h3>Dental Restorations:</h3>
+                    <p class="lead text-start"> We offer a range of dental restorations, including fillings, crowns, bridges, and
+                        dentures, to enhance the appearance and functionality of your smile. We prioritize excellence in
+                        every restoration, aiming for both optimal aesthetics and long-lasting durability. At ASH Dental, we
+                        leverage cutting-edge materials and technology to ensure the highest quality in dental restorations.
+                        Advanced imaging techniques, including 3D technology, allow us to create precise and tailored
+                        solutions for each patient.
+                    </p>
+                    <br/>
+                    <h3>Gum Disease Treatment:</h3>
+                    <p class="lead text-start"> Early detection and intervention are key in managing gum disease. We go
+                        beyond conventional methods to address gum disease comprehensively, from gingivitis to advanced
+                        periodontal stages. We don&#39;t just treat gum disease; we aim to prevent its recurrence.
+                    </p>
+                    <br/>
+                    <h3>Mouthguards and Nightguards:</h3>
+                    <p class="lead text-start"> Customized mouthguards and nightguards provide protection
+                    against teeth grinding and sports-related injuries.
+                    </p>
+                    <br/>
+                    <h3>Emergency Dental Care:</h3>
+                    <p class="lead text-start">We understand that dental emergencies can happen unexpectedly, causing
+                        discomfort and anxiety. Our dedicated Emergency Dental Help services are designed to provide swift
+                        and effective care when you need it the most.
+                    </p>
+                    <p class="lead text-start">Whether you&#39;re dealing with a sudden toothache, a
+                        broken tooth, or any other urgent dental issue, our skilled professionals are here to alleviate your
+                        pain and address your concerns promptly. You deserve focused care during those critical moments.
+                    </p>
+                    <button>click me</button>
+                    <br/>
+                    <h3>Wisdom Teeth Management</h3>
+                    <p class="lead text-start">Our specialized care for wisdom teeth ensures their removal is as comfortable and efficient as
+                    possible. ASH Dental&#39;s commitment to acknowledging patient needs and concerns, along with a
+                    patient-first approach, is evident in our approach to wisdom teeth management. Comfort during the
+                    procedure are not just an expectation; they are a commitment we uphold throughout your dental
+                    journey with us.
+                    </p>
+                    <br/>
+                    <h3>Denture Expertise</h3>
+                    <p class="lead text-start"> Our team is skilled in providing denture solutions that restore smiles and functionality. ASH Dental&#39;s
+                        dedication to embracing uniqueness and acceptance is reflected in our denture expertise. We
+                        understand that every patient is a unique canvas, and our commitment to providing personalized
+                        treatment plans extends to our denture services. Bid farewell to loose dentures; with cutting-edge
+                        technology, you can now enjoy peace of mind by securing your dentures with implants.
+                    </p>
+                    <br/>
+
+            </div>
+
+            <br></br>
+        </div>
+    )
+}
+
+export default GeneralDentistry;
---
+++ src/components/subServices/orthodontics.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import "../../Main.css";
+import { Link, useLocation } from 'react-router-dom';
+
+
+function Orthodontics() {
+    function Navigate({ name, path }) {
+        return (
+            <Link className="nav-item nav-link" to={path} title={name}>
+                {name}
+            </Link>
+        );
+    }
+    return (
+        <div>
+            <hr ></hr>
+            <nav class="navbar nav-bg navbar-expand-sm "> <button class="navbar-toggler" type="button" data-target="#navigation"> <span class="navbar-toggler-icon"></span> </button>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav">
+                        <li class="sub-nav-item active">
+                            <Navigate name={'Cosmetic Dentistry'} path={'/cosmeticDentistry'} /> </li>
+                        <li class="sub-nav-item">
+                            <Navigate name={'Teeth Whitening'} path={'/teethWhitening'} /> </li>
+                        <li class="sub-nav-item"><Navigate name={'Crowns and Veneers'} path={'/crowns'} />  </li>
+                        <li class="sub-nav-item"> <Navigate name={'Dental Implants'} path={'/dental'} />  </li>
+                        <li class="sub-nav-item"><Navigate name={'Orthodontics'} path={'/orthodontics'} /> </li>
+                        <li class="sub-nav-item"> <Navigate name={'General Dentistry'} path={'/generalDentistry'} />  </li>
+                        <li class="sub-nav-item"> <Navigate name={'24 Hour Emergencies'} path={'/emergencies'} /> </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="row serv-top">
+                <div class=" col-7 text-center">
+                    <h1 class="gold text-start">Tooth Alignment and Orthodontic Techniques</h1>
+                    <h3 class="brown text-start ">Precision Orthodontic Care</h3>
+                    <br/>
+                    <p class="lead text-start">We excel in orthodontic care, correcting misalignments and enhancing the overall appearance of
+                    teeth. ASH Dental's commitment to a patient-centric journey ensures that every orthodontic
+                    treatment is tailored to individual patient needs. Access to cutting-edge technology and a research-
+                    driven approach are integral to our orthodontic care, ensuring optimal results.</p>
+                        <br/>
+                        <p class="lead text-start">Whether you&#39;re a teenager embarking on orthodontic care or an adult exploring discreet options, we
+                    provide personalized solutions ranging from traditional braces to clear, nearly invisible alternatives.</p>
+                        <br/>
+                </div>
+
+                <div class="col-5">
+                    <div class="serv2-image-block">
+                        <img class="serv2-image" src="/Images/ServicesLandscape/Ortho2.png" alt="2 description"></img>
+                        <div class="serv2-overlay-block"></div>
+                    </div>
+                </div>
+            </div>
+
+            <br></br>
+
+            <div >
+                <br/>
+                <br/>
+                    <h3></h3>
+
+                    <br/>
+                    <br/>
+                    <h3 ></h3>
+                    <br/>
+
+                <div class="row d-flex justify-content-center">
+                    <div class="col-4" style={{ height: '35%', width: '25%', textAlign: 'center' }}>
+                    <img class="img-fluid" src="/Images/ServicesLandscape/Ortho.png" alt="trainer-img"></img>
+                    </div >
+                    <div className='col-5' style={{ backgroundImage: `url(/Images/Texture3.png)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+                    <div className='pb-4 mb-4 pt-2 mt-2 ps-4 ms-4 pe-4 me-4'>
+                        <br />
+                        <h1 className="card-title text-start">Orthodontic devices employed for aligning teeth.</h1>
+                        <br />
+                        <ul class="lead text-start">
+                        <li>Clear Aligners</li>
+                        <li>Metal braces</li>
+                        <li>clear / ceramic braces</li>
+                    </ul>
+
+                    </div>
+                 </div>
+                </div >
+                <br></br>
+                <div className="row d-flex justify-content-center">
+                <div className='col-5' style={{ backgroundImage: `url(/Images/Texture3.png)`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+                    <div className='pb-4 mb-4 pt-2 mt-2 ps-4 ms-4 pe-4 me-4'>
+                        <br />
+                        <h1 className="card-title text-start">Several kinds of retainers recommended post-treatment.</h1>
+                        <br />
+                        <ul class="lead text-start">
+                                <li className="card-text lead text-start">Clear – invisible retainers/aligner</li>
+                                <li className="card-text lead text-start">Hawley retainer</li>
+                                <li className="card-text lead text-start">Permanent retainer securely bonded to the inner aspect of teeth.</li>
+                            </ul>
+                    </div>
+                </div>
+                
+            <div className="col-4" style={{ height: '35%', width: '25%', textAlign: 'center' }}>
+                <img className="img-fluid" src="/Images/ServicesLandscape/Ortho2.png" alt="trainer-img" />
+            </div >
+            </div >
+            </div>
+
+            <br></br>
+        </div>
+    )
+}
+
+export default Orthodontics;
