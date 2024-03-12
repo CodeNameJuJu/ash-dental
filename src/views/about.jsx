6348bb007f84e8c40c9c218b75bc1b9a8d297629
CodeNameJuJu <julianstone98@gmail.com>
2024-03-12 23:22:19 +0200

    Fixed mapping

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -94,36 +94,89 @@ export const About = () => {
                         {isSmallScreen ? (
                             <>
                                 {imageIndexMapping.map((item, index) => (
-                                    <Card
-                                        key={index}
-                                        name={item.name}
-                                        image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
-                                        title={index === 2 ? "Co-Founder and Dental Artist" : "Founder and Chief Smile Architect"}
-                                        quali={index === 2 ? "BChD (Pret.)" : "BChD (Pret.), DipOdont (Oral Surg.)(Pret.) "}
-                                        bio={index === 2 ? "Driven by a lifelong passion for dentistry, Dr. Eulana isn't just about fixing teeth; she's about crafting confident smiles and empowering individuals to shine. As you step into our practice, Dr. Eulana's warm and caring presence instantly puts you at ease. Her commitment to precision and excellence aligns perfectly with ASH DENTAL's ethos of 'unwavering commitment and a new benchmark in luxury dental care.' This, combined with her calming demeanor, creates a welcoming and stress-free atmosphere where your comfort and well-being are paramount." : "Dr. Rodrigues leads our team with a vision to redefine dental care. As an accomplished dentist with a passion for cosmetic dentistry, Dr Rodrigues has transformed countless smiles and lives."}
-                                    />
+                                    <React.Fragment key={index}>
+                                        {index === 0 && (
+                                            <Card
+                                                name={item.name}
+                                                image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                title="Founder and Chief Smile Architect"
+                                                quali="BChD (Pret.), DipOdont (Oral Surg.)(Pret.)"
+                                                bio="Dr. Rodrigues leads our team with a vision to redefine dental care. As an accomplished dentist with a passion for cosmetic dentistry, Dr Rodrigues has transformed countless smiles and lives."
+                                            />
+                                        )}
+                                        {index === 2 && (
+                                            <Card
+                                                name={item.name}
+                                                image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                title="Co-Founder and Dental Artist"
+                                                quali="BChD (Pret.)"
+                                                bio="Dr. Eulana isn't just about fixing teeth; she's about crafting confident smiles and empowering individuals to shine. As you step into our practice, Dr. Eulana's warm and caring presence instantly puts you at ease. Her commitment to precision and excellence aligns perfectly with ASH DENTAL's ethos of 'unwavering commitment and a new benchmark in luxury dental care."                                            />
+                                        )}
+                                        {index === 1 && (
+                                            <>
+                                                {isSmallScreen ? (
+                                                    <Card
+                                                        name={item.name}
+                                                        image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                        title="Dental Artist"
+                                                        quali="BChD (Pret.)"
+                                                        bio="Meet Dr. Williams, our Dental Artist at ASH Dental. With a passion for crafting beautifully aligned smiles, Dr. Williams brings a blend of technical expertise and artistic flair to dental care."
+                                                    />
+                                                ) : (
+                                                    <Card
+                                                        name={item.name}
+                                                        image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                        title="Co-Founder and Dental Artist"
+                                                        quali="BSc (Pret.), BChD (Pret.)"
+                                                        bio="Meet Dr. Williams, our Dental Artist at ASH Dental. With a passion for crafting beautifully aligned smiles, Dr. Williams brings a blend of technical expertise and artistic flair to dental care."
+                                                    />
+                                                )}
+                                            </>
+                                        )}
+                                    </React.Fragment>
                                 ))}
                             </>
                         ) : (
                             <>
                                 {imageIndexMapping.map((item, index) => (
                                     <React.Fragment key={index}>
-                                        {index === 1 ? (
-                                            <Card2
-                                                name={item.name}
-                                                image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
-                                                title="Co-Founder and Dental Artist"
-                                                quali="BSc (Pret.), BChD (Pret.)"
-                                                bio="Meet Dr. Williams, our Dental Artist at ASH Dental. With a passion for crafting beautifully aligned smiles, Dr. Williams brings a blend of technical expertise and artistic flair to dental care."
-                                            />
-                                        ) : (
+                                        {index === 0 && (
                                             <Card
                                                 name={item.name}
                                                 image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
-                                                title="Dental Artist"
-                                                quali="BChD (Pret.)"
-                                                bio="Dr. Eulana isn't just about fixing teeth; she's about crafting confident smiles and empowering individuals to shine. As you step into our practice, Dr. Eulana's warm and caring presence instantly puts you at ease. Her commitment to precision and excellence aligns perfectly with ASH DENTAL's ethos of 'unwavering commitment and a new benchmark in luxury dental care.'"
+                                                title="Founder and Chief Smile Architect"
+                                                quali="BChD (Pret.), DipOdont (Oral Surg.)(Pret.)"
+                                                bio="Dr. Rodrigues leads our team with a vision to redefine dental care. As an accomplished dentist with a passion for cosmetic dentistry, Dr Rodrigues has transformed countless smiles and lives."
                                             />
+                                        )}
+                                        {index === 2 && (
+                                            <Card
+                                                name={item.name}
+                                                image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                title="Co-Founder and Dental Artist"
+                                                quali="BChD (Pret.)"
+                                                bio="Dr. Eulana isn't just about fixing teeth; she's about crafting confident smiles and empowering individuals to shine. As you step into our practice, Dr. Eulana's warm and caring presence instantly puts you at ease. Her commitment to precision and excellence aligns perfectly with ASH DENTAL's ethos of 'unwavering commitment and a new benchmark in luxury dental care."                                            />
+                                        )}
+                                        {index === 1 && (
+                                            <>
+                                                {isSmallScreen ? (
+                                                    <Card
+                                                        name={item.name}
+                                                        image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                        title="Dental Artist"
+                                                        quali="BChD (Pret.)"
+                                                        bio="Meet Dr. Williams, our Dental Artist at ASH Dental. With a passion for crafting beautifully aligned smiles, Dr. Williams brings a blend of technical expertise and artistic flair to dental care."
+                                                    />
+                                                ) : (
+                                                    <Card
+                                                        name={item.name}
+                                                        image={`${process.env.PUBLIC_URL}/Images/Practice/Low/ASH DENTAL_content_MARCH'24-${item.index}.jpg`}
+                                                        title="Co-Founder and Dental Artist"
+                                                        quali="BSc (Pret.), BChD (Pret.)"
+                                                        bio="Meet Dr. Williams, our Dental Artist at ASH Dental. With a passion for crafting beautifully aligned smiles, Dr. Williams brings a blend of technical expertise and artistic flair to dental care."
+                                                    />
+                                                )}
+                                            </>
                                         )}
                                     </React.Fragment>
                                 ))}
