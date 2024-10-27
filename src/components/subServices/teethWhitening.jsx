3809b8b2a3a9c4222311b803f1842deba2280dee
CodeNameJuJu <julianstone98@gmail.com>
2024-10-27 17:43:12 +0200

    Teeth whitening content updates

--- src/components/subServices/teethWhitening.jsx
+++ src/components/subServices/teethWhitening.jsx
@@ -18,7 +18,7 @@ function TeethWhitening() {
     return (
         <div>
             <hr ></hr>
-            <nav class=" navbar nav-bg navbar-expand-sm navbar-second">
+            <nav className=" navbar nav-bg navbar-expand-sm navbar-second">
                 <button
                     className="navbar-toggler hide-mob"
                     type="button"
@@ -29,51 +29,69 @@ function TeethWhitening() {
                     aria-label="Toggle navigation"
                     onClick={toggleNavbar}
                 >
-                    <i class="fa fa-bars" style={{ 'font-size': '25px' }}></i>
+                    <i className="fa fa-bars" style={{ 'font-size': '25px' }}></i>
                 </button>
 
-                <div class={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="sub-nav-item">
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="sub-nav-item">
                             <Navigate name={'Teeth Whitening'} path={'/teethWhitening'} /> </li>
-                        <li class="sub-nav-item"><Navigate name={'Crowns and Veneers'} path={'/crowns'} />  </li>
-                        <li class="sub-nav-item"> <Navigate name={'Dental Implants'} path={'/dental'} />  </li>
-                        <li class="sub-nav-item"><Navigate name={'Orthodontics'} path={'/orthodontics'} /> </li>
-                        <li class="sub-nav-item"> <Navigate name={'General Dentistry'} path={'/generalDentistry'} />  </li>
-                        <li class="sub-nav-item"> <Navigate name={'24 Hour Emergencies'} path={'/emergencies'} /> </li>
+                        <li className="sub-nav-item"><Navigate name={'Crowns and Veneers'} path={'/crowns'} />  </li>
+                        <li className="sub-nav-item"> <Navigate name={'Dental Implants'} path={'/dental'} />  </li>
+                        <li className="sub-nav-item"><Navigate name={'Orthodontics'} path={'/orthodontics'} /> </li>
+                        <li className="sub-nav-item"> <Navigate name={'General Dentistry'} path={'/generalDentistry'} />  </li>
+                        <li className="sub-nav-item"> <Navigate name={'24 Hour Emergencies'} path={'/emergencies'} /> </li>
                     </ul>
                 </div>
             </nav>
             &nbsp;
             <div className='row'>
                 <div className='col-md-8 col-sm-12'>
-                        <h1 class="gold text-start">Teeth Whitening</h1>
-                        <h3 class="brown text-start ">Illuminate Your Radiance</h3>
-                        {/* <h2 className='brown mb-2 text-start'>Patient-Centric Excellence</h2> */}
+                    <h1 className="gold text-start">Unveil a whiter, brighter smile with our tailored teeth whitening treatment</h1>
                     <br />
-                    <p class="lead text-start">Our Teeth Whitening services are a symphony of artistry and science, meticulously designed to
-                        unveil the true brilliance of your smile. We believe that a radiant smile is a personal expression, and
-                        our approach goes beyond mere aesthetics. Each Teeth Whitening journey begins with a
-                        personalized treatment plan, acknowledging your unique dental needs and preferences. Our
-                        experienced dental professionals blend artistry with precision, ensuring stunning yet natural-looking
-                        results.</p>
+                    <p className="lead text-start">Our Teeth Whitening services at ASH Dental are a blend of artistry and science, designed to
+                        bring out the true brilliance of your smile. We understand that a radiant smile is more than
+                        just an aesthetic feature; it’s a personal expression. Each teeth whitening journey starts with
+                        a personalized treatment plan that caters to your unique dental needs and preferences.
+                    </p>
                     <br />
-                    <p class="lead text-start">Leveraging advanced technology and safe whitening agents, our procedures are crafted to remove
-                        stains and discoloration effectively, prioritizing your dental health and comfort.</p>
+                    <p className="lead text-start">
+                        Our experienced dental professionals combine artistry with precision to achieve stunning yet
+                        natural-looking results. Using advanced technology and safe whitening agents, we
+                        effectively remove stains and discoloration while prioritizing your dental health and comfort.
+                    </p>
                     <br />
-                    <p class="lead text-start" >Our commitment to enduring and beautiful results, combined with the latest in whitening
-                        technology.</p>
+                    <p className="lead text-start" >With a commitment to delivering enduring and beautiful results, we utilize the latest
+                        whitening techniques to enhance your smile.
+                    </p>
                     <br />
-                    <p class="lead text-start">Illuminate your unique radiance by scheduling a consultation and discovering the art of Teeth
-                        Whitening at ASH Dental.</p>
+                    <p className="lead text-start">Illuminate your unique radiance by scheduling a consultation with us at ASH Dental and
+                        experience the art of expert teeth whitening.
+                    </p>
+                    <br />
+                    <h2 className="gold text-start">What is teeth whitening and what are the benefits?</h2>
+                    <br />
+                    <p className="lead text-start">
+                        Teeth whitening is a cosmetic dental procedure designed to lighten the colour of your teeth
+                        and remove stains and discoloration. It involves using safe, effective whitening agents to
+                        break down and eliminate stains caused by factors like coffee, tea, red wine, tobacco, and
+                        aging. The procedure takes place in our comfortable dental chair, where you can relax and
+                        enjoy your favourite movie or series on our ceiling-mounted TV.
+                    </p>
+                    <br />
+                    <p className="lead text-start">
+                        The benefits of teeth whitening extend beyond just an enhanced appearance. A brighter
+                        smile can boost your confidence and self-esteem, making you feel more comfortable in
+                        social and professional settings. Additionally, a whiter smile can make you look younger and
+                        more vibrant. At ASH Dental, our expert teeth whitening treatments not only help you
+                        achieve a radiant smile but also contribute to improved overall dental health by removing
+                        surface stains that can potentially lead to more significant oral health issues.
+                    </p>
                     <br />
                 </div>
-                <img className='col-md-4 col-sm-12 mb-md-0 mb-3' src="/Images/ServicesLandscape/TW1.webp" alt="Teeth whitening" style={{ height: '100%' }} />
+                <img className='col-md-4 col-sm-12 pt-5 mt-5 mb-md-0 mb-3' src="/Images/ServicesLandscape/TW1.webp" alt="Teeth whitening" style={{ height: '100%' }} />
             </div>
             &nbsp;
-
-
-
         </div>
     )
 }
